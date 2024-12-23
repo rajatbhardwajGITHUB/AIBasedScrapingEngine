5a91bd58590c6d9e5aa666d2e309f1834fbdb6a1
QuestPvtLtd <questranddpvtltd2@gmail.com>
2024-12-23 06:24:17 +0530

    removed unwanted fields from the pydantic model

--- app/models/models.py
+++ app/models/models.py
@@ -2,11 +2,6 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 class ExtractedData(BaseModel):
-    title: Optional[str]
-    headers: List[str]
-    about_section: Optional[str]
-    services_section: Optional[str]
-    meta_description: Optional[str]
     industry: Optional[str]
     company_size: Optional[str]
     location: Optional[str]
